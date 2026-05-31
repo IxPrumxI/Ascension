7ee85ef42a139d8abe6e2b044273273f58285cbc
relawiii <relawiime@gmail.com>
2026-05-31 09:48:15 +0800

    26.1.2 at ver1

---
+++ common/src/main/java/com/discordsrv/common/config/connection/BotConfig.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of DiscordSRV, licensed under the GPLv3 License
+ * Copyright (c) 2016-2026 Austin "Scarsz" Shapiro, Henri "Vankka" Schubin and DiscordSRV contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.discordsrv.common.config.connection;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
+
+@ConfigSerializable
+public class BotConfig {
+
+    public static final String DEFAULT_TOKEN = "Token here";
+
+    @Comment("The Discord bot token from https://discord.com/developers/applications\n"
+            + "Requires a connection to: discord.com, gateway.discord.gg, cdn.discordapp.com\n"
+            + "Privacy Policy: https://discord.com/privacy Terms: https://discord.com/developers/docs/policies-and-agreements/developer-terms-of-service")
+    public String token = DEFAULT_TOKEN;
+
+}
